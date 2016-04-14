3b5dfb3afa5d220061675e5efea5f17b5dbdd72b
dbboling <dbboling@gmail.com>
2016-04-14 14:07:38 -0700

    Update aria_accordion.js
    Now has the ability to use TAB and ENTER to control accordion. Also added the ability to collapse all tabs for easier accessibility using keyboard controls.

--- aria_accordion.js
+++ aria_accordion.js
@@ -1,8 +1,30 @@
-$(document).ready(function($) {
-  $('#accordion').find('.accordion-toggle').click(function(){  // When ".accordion-toggle" is clicked
-    $(this).parent().find('.accordion-content').css('display','none');  // Hide everything in everything in ".accordion-content" classes
-    $(this).parent().find('.accordion-toggle').attr('aria-hidden', 'true');  //  Let aria know the content inside ".accordion-toggle" is hidden
-    $(this).find('.accordion-content').css('display','block');  // Show the .accordion-content that was clicked
-    $(this).attr('aria-hidden', 'false');  // Let aria know it's showing
+  $(document).ready(function($) {
+    $('#accordion').find('.accordion-toggle').click(function(){
+
+      //If aria-hidden is set to true, expand it and collapse others
+      if($(this).attr('aria-hidden') == 'true'){
+
+      //Expand or collapse this panel
+        $(this).parent().find('.accordion-content').css('display','none');
+        $(this).parent().find('.accordion-toggle').attr('aria-hidden', 'true');
+        $(this).find('.accordion-content').css('display','block');
+        $(this).attr('aria-hidden', 'false');
+    }
+
+    //If aria-hidden is set to false, collapse it
+    else if($(this).attr('aria-hidden') == 'false'){
+        $(this).parent().find('.accordion-content').css('display','none');
+        $(this).parent().find('.accordion-toggle').attr('aria-hidden', 'true');
+    }
+
+    //Do nothing if aria-hidden isn't set
+    else{}
   });
+        
+    //Allow the enter key to function as left click
+    $('.accordion-toggle').keyup(function(event){
+        if(event.keyCode == 13){
+            $(this).click();
+        }
+    });
 });
